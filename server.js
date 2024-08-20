4ad5dbbdb0a62ad6380e304beac40cfafdd2abea
Jakub Durkáč <xdurkac1@fi.muni.cz>
2024-08-20 17:10:50 +0200

    Add data structures to store active client names, client info and tables(games) info. Server now handles initial message from client and basic client disconnects.

--- server.js
+++ server.js
@@ -8,17 +8,99 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const activeNames = [];
+const nameToClientInfo = {};
+// const exampleClientInfo = {
+//     socket: WebSocket,
+//     buyInPrice: number,
+//     bigBlindPrice: number
+//     tableName: string | null,
+// };
+const tables = [];
+// const exampleTable = {
+//     tableName: string,
+//     playerNames: string[],
+//     buyInPrice: number,
+//     bigBlindPrice: number
+// };
+
+function sendTablesList(toName) {
+    const clientInfo = nameToClientInfo[toName];
+    if (!clientInfo || !clientInfo.socket) {
+        return;
+    }
+    
+    const message = {
+        type: "tablesList",
+        data: tables
+    };
+
+    clientInfo.socket.send(JSON.stringify(message));
+}
+
+function handleClientDisconnect(name) {
+    // todo - notify opponents if client has assigned tableName, update table, etc.
+    delete nameToClientInfo[name];
+    removeName(name);
+}
+
+function removeName(name) {
+    const indexToRemove = activeNames.indexOf(name);
+    if (indexToRemove >= 0) {
+        activeNames.splice(indexToRemove, 1);
+    }
+}
+
 wss.on('connection', (ws) => {
     console.log(`A new client connected.`);
 
-    ws.on('message', (message) => {     
-        // const strMessage = message.toString();
-        // const objMessage = JSON.parse(strMessage);
-        // ws.send(JSON.stringify({notification: 'example'}));
+    ws.on('message', (message) => {
+        let objMessage;
+        try {
+            objMessage = JSON.parse(message.toString());
+        } catch (error) {
+            console.error('JSON parsing failed:', error);
+        }
+        
+        if (!objMessage || !objMessage.type) {
+            console.log('Invalid message structure.');
+            return;
+        }
+
+        switch (objMessage.type) {
+            case "initial":
+                const { clientName, buyInPrice, bigBlindPrice } = objMessage.data;
+                if (activeNames.includes(clientName)) {
+                    ws.send(JSON.stringify({type: 'duplicate'}));
+                    return;
+                }
+
+                activeNames.push(clientName);
+                nameToClientInfo[clientName] = {
+                    socket: ws,
+                    buyInPrice: buyInPrice,
+                    bigBlindPrice: bigBlindPrice,
+                    tableName: null, // to be assigned after client joins a table
+                };
+                sendTablesList(clientName);
+
+                console.log(`Client ${clientName} joins matchmaking.`);
+
+            default:
+                break;
+        }
     });
 
     ws.on('close', (code, reason) => {
-        console.log('Client disconnected.');
+        for (let i = 0; i < activeNames.length; i++) {
+            const name = activeNames[i];
+            const clientInfo = nameToClientInfo[name];
+            if (clientInfo && clientInfo.socket === ws) {
+                console.log(`Client ${name} disconnected. Code: ${code}, Reason: ${reason}`);
+                handleClientDisconnect(name);
+                break;
+            }
+        }
     });
 });
 
